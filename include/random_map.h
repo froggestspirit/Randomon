c7a131a2fd7feed6128c9da3b4bab45db15337cb
froggestspirit <froggestspirit@gmail.com>
2021-09-18 17:20:15 -0400

    map work

--- include/random_map.h
+++ include/random_map.h
@@ -5,15 +5,18 @@
 #include "main.h"
 
 #define EMPTY_TILE 0x01010101
+#define PATH_TILE 0x01010101
+
 #define MAX_DIM 30
 #define MARGIN_SIZE 2
 #define PATH_LINEARITY 4 // Paths will move a minimum of this number of tiles before turning (unless it's destination is closer)
+#define MAX_REROLLS 10
+#define NUM_BLOCKS 6
 
 u16 rand(int, int);
 bool8 check_rect(u8, u8, u8, u8, u8);
 void fill_rect(u8, u8, u8, u8, u8);
 bool8 draw_line(u8, u8, u8, u8, u8);
-void process_path(u8, struct Point, struct Point);
 void RandomMap(u16, u8, bool8);
 
 
--- src/random_map.c
+++ src/random_map.c
@@ -25,8 +25,8 @@ u8 mapWidth, mapHeight;
 bool8 badMap;
 
 struct Point{
-    u8 x;
-    u8 y;
+    s8 x;
+    s8 y;
 };
 
 extern struct BackupMapLayout gBackupMapLayout;
@@ -39,6 +39,39 @@ struct ConnectionFlags
     u8 west:1;
     u8 east:1;
 };
+
+static const u32 randMapBlock[16 * NUM_BLOCKS] = {
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+
+PATH_TILE, PATH_TILE, PATH_TILE, PATH_TILE,
+PATH_TILE, PATH_TILE, PATH_TILE, PATH_TILE,
+PATH_TILE, PATH_TILE, PATH_TILE, PATH_TILE,
+PATH_TILE, PATH_TILE, PATH_TILE, PATH_TILE,
+
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE,
+EMPTY_TILE, EMPTY_TILE, EMPTY_TILE, EMPTY_TILE
+};
+
 
 u16 rand(int a, int b){  // Psuedo Random number generator, using the same formula as the Gen 3/4 games
     currentSeed = ((0x41C64E6D * currentSeed) + 0x00006073) & 0xFFFFFFFF;
@@ -129,38 +162,38 @@ bool8 draw_line(u8 tile, u8 x1, u8 y1, u8 x2, u8 y2){  // Draw a line of tiles (
     return FALSE;
 }
 
-void process_path(u8 dir, struct Point point, struct Point destPoint){  // Handle drawing the paths
+void process_path(u8 dir, struct Point *point, struct Point *destPoint){  // Handle drawing the paths
     s8 len;
     u8 xy[2];
     u8 xy2[2];
-    while(point.x != destPoint.x || point.y != destPoint.y){
+    while(point->x != destPoint->x || point->y != destPoint->y){
         if(!dir){  // x
-            if(abs(destPoint.x - point.x) > PATH_LINEARITY)
-                len = rand(PATH_LINEARITY, abs(destPoint.x - point.x));
+            if(abs(destPoint->x - point->x) > PATH_LINEARITY)
+                len = rand(PATH_LINEARITY, abs(destPoint->x - point->x));
             else
-                len = abs(destPoint.x - point.x);
+                len = abs(destPoint->x - point->x);
             if(len){
-                xy[0] = xy2[0] = point.x;
-                xy[1] = xy2[1] = point.y;
-                if(destPoint.x < point.x)
+                xy[0] = xy2[0] = point->x;
+                xy[1] = xy2[1] = point->y;
+                if(destPoint->x < point->x)
                     len = -len;
                 xy2[0] += len;
-                point.x += len;
+                point->x += len;
                 if(draw_line(2, xy[0], xy[1], xy2[0], xy2[1]))
                     point = destPoint;
             }
         }else{  // y
-            if(abs(destPoint.y - point.y) > PATH_LINEARITY)
-                len = rand(PATH_LINEARITY, abs(destPoint.y - point.y));
+            if(abs(destPoint->y - point->y) > PATH_LINEARITY)
+                len = rand(PATH_LINEARITY, abs(destPoint->y - point->y));
             else
-                len = abs(destPoint.y - point.y);
+                len = abs(destPoint->y - point->y);
             if(len){
-                xy[0] = xy2[0] = point.x;
-                xy[1] = xy2[1] = point.y;
-                if(destPoint.y < point.y)
+                xy[0] = xy2[0] = point->x;
+                xy[1] = xy2[1] = point->y;
+                if(destPoint->y < point->y)
                     len = -len;
                 xy2[1] += len;
-                point.y += len;
+                point->y += len;
                 if(draw_line(2, xy[0], xy[1], xy2[0], xy2[1]))
                     point = destPoint;
             }
@@ -173,9 +206,9 @@ void RandomMap(u16 seed, u8 mapType, bool8 newGame){
     int i;
     u8 x, y;
     u16 size;
-    u8 exitPos[4];
-    u8 exitSize[4];
-    u8 mapDim[4];
+    static u8 exitPos[4];
+    static u8 exitSize[4];
+    static u8 mapDim[4];
     badMap = FALSE;
     currentSeed = seed;
     // Maps are built in blocks first that represent 4x4 tiles
@@ -203,6 +236,9 @@ void RandomMap(u16 seed, u8 mapType, bool8 newGame){
     }
 
     // Create a bare map template
+    for(i = 0; i < MAX_MAP_DATA_SIZE >> 6; i++)
+        gRandomMapData[i] = 0;
+
     for(y = 0; y < mapHeight; y++){
         u16 yOff = y * mapWidth;
         for(x = 0; x < mapWidth; x++){
@@ -227,25 +263,25 @@ void RandomMap(u16 seed, u8 mapType, bool8 newGame){
                 case 4:  // center
                     gRandomMapData[yOff + x] = 0;
                     break;
-                case 1:
+                case 1:  // north
                     if((x >= exitPos[0]) && (x < exitPos[0] + exitSize[0]))
                         gRandomMapData[yOff + x] = 0;
                     else
                         gRandomMapData[yOff + x] = 1;
                     break;
-                case 3:
+                case 3:  // west
                     if((y >= exitPos[1]) && (y < exitPos[1] + exitSize[1]))
                         gRandomMapData[yOff + x] = 0;
                     else
                         gRandomMapData[yOff + x] = 1;
                     break;
-                case 5:
+                case 5:  // east
                     if((y >= exitPos[3]) && (y < exitPos[3] + exitSize[3]))
                         gRandomMapData[yOff + x] = 0;
                     else
                         gRandomMapData[yOff + x] = 1;
                     break;
-                case 7:
+                case 7:  // south
                     if((x >= exitPos[2]) && (x < exitPos[2] + exitSize[2]))
                         gRandomMapData[yOff + x] = 0;
                     else
@@ -254,86 +290,133 @@ void RandomMap(u16 seed, u8 mapType, bool8 newGame){
             }
         }
     }
-    /*// Set focal points for the path
+    // Set focal points for the path
     // Create one near each exit, and one near the center
-    pathPointCenter = [int(mapWidth / 2), int(mapHeight / 2)]
-    pathPoint = []
-    firstDir = []
-    xyAvg = [0, 0]
-    points = 0
+    static struct Point *pathPointCenter;
+    static struct Point *xyAvg;
+    static struct Point *pathPoint[10]; 
+    static u8 firstDir[10];
+    static u8 points = 0;
+    pathPointCenter->x = mapWidth >> 1;
+    pathPointCenter->y = mapHeight >> 1;
+    xyAvg->x = 0;
+    xyAvg->y = 0;
 
-    if exitPos[0] > 0:
-        pathPoint.append([exitPos[0] + int(exitSize[0] / 2), MARGIN_SIZE])
-        firstDir.append(1)
-        xyAvg[0] -= pathPoint[points][0]
-        xyAvg[1] -= pathPoint[points][1]
-        points += 1
-    if exitPos[1] > 0:
-        pathPoint.append([MARGIN_SIZE, exitPos[1] + int(exitSize[1] / 2)])
-        firstDir.append(0)
-        xyAvg[0] -= pathPoint[points][0]
-        xyAvg[1] -= pathPoint[points][1]
-        points += 1
-    if exitPos[2] > 0:
-        pathPoint.append([exitPos[2] + int(exitSize[2] / 2), mapHeight - (MARGIN_SIZE + 1)])
-        firstDir.append(1)
-        xyAvg[0] -= pathPoint[points][0]
-        xyAvg[1] -= pathPoint[points][1]
-        points += 1
-    if exitPos[3] > 0:
-        pathPoint.append([mapWidth - (MARGIN_SIZE + 1), exitPos[3] + int(exitSize[3] / 2)])
-        firstDir.append(0)
-        xyAvg[0] -= pathPoint[points][0]
-        xyAvg[1] -= pathPoint[points][1]
-        points += 1
+    if(exitPos[0] > 0){
+        pathPoint[points]->x = exitPos[0] + (exitSize[0] >> 1);
+        pathPoint[points]->y = MARGIN_SIZE;
+        firstDir[points] = 1;
+        xyAvg->x -= pathPoint[points]->x;
+        xyAvg->y -= pathPoint[points]->y;
+        points++;
+    }
+    if(exitPos[1] > 0){
+        pathPoint[points]->x = MARGIN_SIZE;
+        pathPoint[points]->y = exitPos[1] + (exitSize[1] >> 1);
+        firstDir[points] = 0;
+        xyAvg->x -= pathPoint[points]->x;
+        xyAvg->y -= pathPoint[points]->y;
+        points++;
+    }
+    if(exitPos[2] > 0){
+        pathPoint[points]->x = exitPos[2] + (exitSize[2] >> 1);
+        pathPoint[points]->y = mapHeight - (MARGIN_SIZE + 1);
+        firstDir[points] = 1;
+        xyAvg->x -= pathPoint[points]->x;
+        xyAvg->y -= pathPoint[points]->y;
+        points++;
+    }
+    if(exitPos[3] > 0){
+        pathPoint[points]->x = mapWidth - (MARGIN_SIZE + 1);
+        pathPoint[points]->y = exitPos[3] + (exitSize[3] >> 1);
+        firstDir[points] = 0;
+        xyAvg->x -= pathPoint[points]->x;
+        xyAvg->y -= pathPoint[points]->y;
+        points++;
+    }
 
     // Adjust the center point so there's less blank space
-    if(points):
-        xyAvg[0] = int(xyAvg[0] / points) - 1
-        xyAvg[1] = int(xyAvg[1] / points) - 1
-        if(xyAvg[0] < MARGIN_SIZE):
-            xyAvg[0] += mapWidth
-        if(xyAvg[1] < MARGIN_SIZE):
-            xyAvg[1] += mapHeight
-        pathPointCenter = [xyAvg[0], xyAvg[1]]
+    if(points){
+        xyAvg->x /= points;
+        xyAvg->y /= points;
+        xyAvg->x--;
+        xyAvg->y--;
+        if(xyAvg->x < MARGIN_SIZE)
+            xyAvg->x += mapWidth;
+        if(xyAvg->y < MARGIN_SIZE)
+            xyAvg->y += mapHeight;
+        pathPointCenter = xyAvg;
+    }
 
     // Create buildings or entities that should spawn a path point
-    entity = [4, 5] // Example Pokemon Center, then Mart
-    entityPos = []
+    #define NUM_ENTITY 2
+    u8 entity[NUM_ENTITY];
+    entity[0] = 4;  // Pokemon Center
+    entity[1] = 5;  // Mart
+    struct Point *entityPos[NUM_ENTITY];
 
     // Draw the path with the path points
-    for i, point in enumerate(pathPoint):
-        process_path(firstDir[i], point, pathPointCenter)
-    gRandomMapData[(pathPointCenter[1] * mapWidth) + pathPointCenter[0]] = 2
+    for(i = 0; i < points; i++)
+        process_path(firstDir[i], pathPoint[i], pathPointCenter);
+    gRandomMapData[(pathPointCenter->y * mapWidth) + pathPointCenter->x] = 2;
 
     // Find space for the entities
-    for i, obj in enumerate(entity):
-        entityPos.append([0,0])
-        xy = [0,0]
-        rerolls = 0
-        while entityPos[i] == [0,0]:
-            if(rerolls > MAX_REROLLS): // Too many re-rolls, abort
-                entityPos[i] = [1,1]
-                badMap = TRUE
-            else:
-                rerolls += 1
-                xy[0] = rand(MARGIN_SIZE, (mapWidth - 1) - MARGIN_SIZE)
-                xy[1] = rand(MARGIN_SIZE, (mapHeight - 2) - MARGIN_SIZE)
-                dprint(f"Re-roll entity pos:{seed} ({MARGIN_SIZE}, {(mapWidth - 1) - MARGIN_SIZE}) x ({MARGIN_SIZE}, {(mapHeight - 2) - MARGIN_SIZE}) : {xy}")
-                if(xy[0] == pathPointCenter[0]):
-                    xy[0] += 1
-                if(xy[1] == pathPointCenter[1]):
-                    xy[1] += 1
-                if(check_rect(0, xy[0], xy[1] - 1, 1, 2)): // Make sure the area the entity is placed is clear, and the tile above it
-                    if(check_rect(2, xy[0], xy[1] + 1, 1, 1)): // Do a separate check for the tile below the entity, alowing paths, since they are good to lead up to buildings
-                        entityPos[i] = [xy[0], xy[1]]
-                        gRandomMapData[(xy[1] * mapWidth) + xy[0]] = obj
-    
+    for(i = 0; i < NUM_ENTITY; i++){
+        entityPos[i]->x = entityPos[i]->y = 0;
+        s8 xy[2];
+        xy[0] = xy[1] = 0;
+        u8 rerolls = 0;
+        while(entityPos[i]->x == 0 && entityPos[i]->y == 0){
+            if(rerolls > MAX_REROLLS){ // Too many re-rolls, abort
+                entityPos[i]->x = entityPos[i]->y = 1;
+                badMap = TRUE;
+            }else{
+                rerolls++;
+                xy[0] = rand(MARGIN_SIZE, (mapWidth - 1) - MARGIN_SIZE);
+                xy[1] = rand(MARGIN_SIZE, (mapHeight - 2) - MARGIN_SIZE);
+                if(xy[0] == pathPointCenter->x)
+                    xy[0]++;
+                if(xy[1] == pathPointCenter->y)
+                    xy[1]++;
+                if(check_rect(0, xy[0], xy[1] - 1, 1, 2)){ // Make sure the area the entity is placed is clear, and the tile above it
+                    if(check_rect(2, xy[0], xy[1] + 1, 1, 1)){ // Do a separate check for the tile below the entity, alowing paths, since they are good to lead up to buildings
+                        entityPos[i]->x = xy[0];
+                        entityPos[i]->y = xy[1];
+                        gRandomMapData[(xy[1] * mapWidth) + xy[0]] = entity[i];
+                    }
+                }
+            }
+        }
+    }
+
     // Create paths for the entities
-    for i, point in enumerate(entityPos):
-        point.y += 1
-        process_path(0, point, pathPointCenter)*/
+    for(i = 0; i < NUM_ENTITY; i++){
+        entityPos[i]->y++;
+        process_path(0, entityPos[i], pathPointCenter);
+    }
 
+    // Copy over actual tile data
+    u32 *tilePointer[16];
+    for(i = 0; i < 16; i++){
+        tilePointer[i] = gBackupMapLayout.map;
+        tilePointer[i] += (i >> 2) + mapWidth;
+        tilePointer[i] += (i & 3);
+    }
+    for(y = 0; y < mapHeight; y++){
+        u16 yOff = y * mapWidth;
+        for(x = 0; x < mapWidth; x++){
+            for(i = 0; i < 16; i++){
+                *tilePointer[i] = randMapBlock[(gRandomMapData[yOff + x] << 4) + i];
+                tilePointer[i] += 4;
+            }
+            
+        }
+        for(i = 0; i < 16; i++)
+            tilePointer[i] += 3 * mapWidth;
+    }
 
+    
+    gBackupMapLayout.width = mapWidth << 2;
+    gBackupMapLayout.height = mapHeight << 2;
 
 }
